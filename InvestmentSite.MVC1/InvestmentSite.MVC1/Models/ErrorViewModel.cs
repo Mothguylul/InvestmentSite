17d93a01ee2db668ed8dfda6582ffaf66a71d33f
mothguylul <juliusmuench89gmail.com>
2024-09-25 18:22:18 +0200

    Old Site back, calculaion func working

--- InvestmentSite.MVC1/InvestmentSite.MVC1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InvestmentSite.MVC1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
