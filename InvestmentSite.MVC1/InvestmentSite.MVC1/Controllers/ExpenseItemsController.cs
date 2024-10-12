fd1be8828c12e26404634454a4ca23dc83c83181
mothguylul <juliusmuench89gmail.com>
2024-10-12 17:47:24 +0200

    fixing error

---
+++ InvestmentSite.MVC1/InvestmentSite.MVC1/Controllers/ExpenseItemsController.cs
@@ -0,0 +1,25 @@
+﻿using InvestmentSite.MVC1.Database;
+using InvestmentSite.MVC1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace InvestmentSite.MVC1.Controllers
+{
+    public class ExpenseItemsController : Controller
+    {
+        private readonly InvestmentContext _context;
+
+        public ExpenseItemsController(InvestmentContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetExpenseItem(int id)
+        {
+            ExpenseItem? findId = _context.ExpenseItems.Where(e => e.Id == id).FirstOrDefault();
+
+            return View("GetExpenseItem");
+                   
+        }
+    }
+}
